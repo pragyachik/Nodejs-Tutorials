695b4f9efe2dd889aeca16c72ac90622a18c7e1f
icyfig <icyfig@gmail.com>
2022-07-05 22:27:28 +0530

    Added Nextjs link

---
+++ nextjsapp/nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+export default function FirstPost() {
+    return (
+        <>
+            <h1>First Post</h1>
+            <h2 className="title">
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+            <style jsx>
+                {`
+                    .title a {
+                        color: #0070f3;
+                        text-decoration: none;
+                      }
+              
+                      .title a:hover,
+                      .title a:focus,
+                      .title a:active {
+                        text-decoration: underline;
+                      }
+              
+                      .title {
+                        margin: 0;
+                        line-height: 1.15;
+                        font-size: 4rem;
+                      }
+                `}
+            </style>
+        </>
+    );
+  }  
