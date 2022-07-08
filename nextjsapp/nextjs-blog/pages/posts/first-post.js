91a982b129b6c83179a27aa6c5a47d0199856cc4
Pragya Agrawal <pragyachik@gmail.com>
2022-07-09 00:53:41 +0530

    initial page layout test

--- nextjsapp/nextjs-blog/pages/posts/first-post.js
+++ nextjsapp/nextjs-blog/pages/posts/first-post.js
@@ -1,48 +1,57 @@
 import Link from "next/link";
 
+function Navigation(){
+    return (
+        <>
+            <nav className="flex bg-teal-600 drop-shadow-lg h-16 items-center text-lg text-pink-50 font-mono">
+                <div className="basis-1/4 justify-center px-20">
+                    <div><a>Company Name</a></div>
+                </div>
+                <div className="basis-1/2"></div>
+                <div className="flex basis-1/4 justify-around px-5">
+                    <div>
+                        <Link href="/">
+                            <a>Home</a>
+                        </Link>
+                    </div>
+                    <div><a>About</a></div>
+                    <div><a>Posts</a></div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+function Body(){
+    return (
+        <>
+            <div className="flex flex-col items-center ml-64 mr-64 mt-8 mb-8 p-3 bg-white border-2 border-teal-600">
+                <div className="mb-8">
+                    <h1 className="text-3xl font-bold underline">
+                    Hello world!
+                    </h1>
+                </div>
+                <div>
+                    <p>Welcome to this website! We are testing Tailwind CSS. Please click the button below if you like the site</p>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                    Like!
+                    </button>
+                </div>
+                <div>
+                    -{` `}Developer P
+                </div>
+            </div>
+        </>
+    )
+}
+
 export default function FirstPost() {
     return (
         <>
-            <nav className="bg-teal-500 drop-shadow-lg h-16">
-                <a>Test</a>
-                <a>Test</a>
-                <a>Test</a>
-            </nav>
-            <h1 className="text-3xl font-bold underline">
-            Hello world!
-            </h1>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-            Button
-            </button>
-            <h1>First Post</h1>
-            <h2 className="title">
-                <Link href="/">
-                    <a>Back to home</a>
-                </Link>
-            </h2>
-            <style jsx>
-                {`
-                    .title a {
-                        color: orange;
-                        text-decoration: none;
-                      }
-              
-                      .title a:hover,
-                      .title a:focus,
-                      .title a:active {
-                        text-decoration: underline;
-                      }
-              
-                      .title {
-                        margin: 0;
-                        line-height: 1.15;
-                        font-size: 1rem;
-                      }
-                    .nav{
-                        background-color:gray;
-                    }
-                `}
-            </style>
+            <div className="bg-teal-100 h-screen w-screen">
+                <Navigation />
+                <Body />
+            </div>
         </>
     );
   }  
