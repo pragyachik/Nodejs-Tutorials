52264a5e67fdea46b4caf2c620190bd57baf4733
icyfig <icyfig@gmail.com>
2022-07-11 22:52:13 +0530

    Added Title

--- nodejs/index.js
+++ nodejs/index.js
@@ -7,7 +7,7 @@ app.set('view engine','ejs')
 app.get('/', (req,res)=>{
     // res.send("<p>Good Evening M</p>")
     //res.sendFile('./views/home.html',{root:__dirname})
-    res.render('home')
+    res.render('home',{title:'Home'})
 });
 
 app.get('/about', (req,res)=>{
@@ -21,7 +21,7 @@ app.get('/aboutus',(req,res)=>{
 
 app.use((req,res)=>{
     // res.status(301).sendFile('views/404.html',{root:__dirname})
-    res.render('404')
+    res.render('404',{title:'404'})
 })
 app.listen(4000);
 
